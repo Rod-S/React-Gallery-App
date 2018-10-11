c2632d6d3b15af316d6e834c11685ecdc90b0c8c
Rod-S <33836692+Rod-S@users.noreply.github.com>
2018-10-11 14:42:51 -0500

    -Gallery component updating with new axios fetch data on each NavLink click

--- src/App.js
+++ src/App.js
@@ -1,66 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import axios from 'axios';
 
-import Header from './Components/Header';
-import Nav from './Components/Nav';
-import Gallery from './Components/Gallery';
-import apiKey from './.config';
-import Search from './Components/Search';
+import Container from './Components/Container';
 
-class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      photos: [],
-      loading: true,
-    };
-  }
-
-  componentDidMount() {
-    this.performSearch('cat');
-  }
-
-  performSearch = (tags) => {
-      axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=?&tags=${tags}&safe_search=1&per_page=24&page=1&api_key=${apiKey}`)
-        .then(response => {
-          this.setState(prevState => ({
-            photos: response.data.photos.photo,
-            loading: false
-            })
-          );
-          console.log(response.data.photos.photo);
-        })
-        .catch(error => {
-          console.log('Error fetching and parsing data', error);
-        });
-    }
-
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="container">
-          {/*<Header
-            onSearch={this.performSearch}
-          /> */}
-          <Search
-            onSearch={this.performSearch}
-          />
-          <Nav />
-          {
-            (this.state.loading)
-              ? <p>Loading...</p>
-              :
-              <Gallery
-                data={this.state.photos}
-              />
-          }
-        </div>
-      </BrowserRouter>
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Switch>
+        <Route
+          path ='/:id'
+          render={(props) => <Container {...props} /> }
+        />
+      </Switch>
+    </div>
+  </BrowserRouter>
     );
-  }
-}
 
 export default App;
